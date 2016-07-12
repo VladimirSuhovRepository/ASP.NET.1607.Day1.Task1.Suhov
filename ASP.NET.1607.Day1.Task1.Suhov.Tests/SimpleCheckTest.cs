eea4abe24eb4c89b7025a7a0c7f6d85414af90db
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-12 00:59:14 -0700

    first commit

---
+++ ASP.NET.1607.Day1.Task1.Suhov.Tests/SimpleCheckTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ASP.NET._1607.Day1.Task1.Suhov.Tests
+{
+    [TestClass]
+    public class SimpleCheckTest
+    {
+        [TestMethod]
+        public void TestIsSimpleSimpleValues()
+        {
+            // Arrange
+            var arr = new List<int>() { 2, 3, 5, 7, 11, 13 };
+            // Act
+            // Assert
+            arr.ForEach(delegate (int numb) {
+                Assert.IsTrue(SimpleCheck.IsSimple(numb));
+            });
+        }
+        [TestMethod]
+        public void TestIsSimpleNotSimpleValues()
+        {
+            // Arrange
+            var arr = new List<int>() { 4, 6, 8, 9, 10, 14 };
+            // Act
+            // Assert
+            arr.ForEach(delegate (int numb) {
+                Assert.IsFalse(SimpleCheck.IsSimple(numb));
+            });
+        }
+
+    }
+}
---
+++ ASP.NET.1607.Day1.Task1.Suhov/SimpleCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET._1607.Day1.Task1.Suhov
+{
+    public static class SimpleCheck
+    {
+        /// <summary>Checking whether number is simple</summary> 
+        /// <param name="value">Number which should be checked</param> 
+        /// <returns>If the number <paramref name="n"/> is simple, result:(<paramref name="value"/>) will be true</returns> 
+        public static bool IsSimple(int numb)
+        {
+            for (int i = 2; i < numb; i++)
+            {
+                if (numb % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
